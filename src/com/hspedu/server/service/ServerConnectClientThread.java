b881259cb63d88714d39c82fc6b51ad952866842
SeanHuang <sean02050923@gmail.com>
2024-01-20 10:31:06 +0800

    rewrite some comments in proper English

--- src/com/hspedu/server/service/ServerConnectClientThread.java
+++ src/com/hspedu/server/service/ServerConnectClientThread.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 
 public class ServerConnectClientThread extends Thread {
     private Socket socket;
-    private String userID;//the user whose ID connected to the server
+    private String userID; // The user ID of the client connected to the server
 
     public ServerConnectClientThread(Socket socket, String userID) {
         this.socket = socket;
@@ -18,44 +18,44 @@ public class ServerConnectClientThread extends Thread {
 
     @Override
     public void run() {
-
         while (true) {
-            //accept info
             try {
-                System.out.println(" keep connecting with client: " + userID);
+                // Maintaining the connection with the client
+                System.out.println("Continuously connected with client: " + userID);
                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
                 Message message = (Message) ois.readObject();
 
-                //send online friends list back
+                // Processing request for online friends list
                 if (message.getMesType().equals(MessageType.MESSAGE_GET_ONLINE_FRIENDS)) {
-                    System.out.println(message.getSender() + " requests a online friends List");
-                    //create a message and set its content according to hm
+                    System.out.println(message.getSender() + " requests the online friends list.");
+                    // Create a message and set its content
                     Message mes = new Message();
                     mes.setMesType(MessageType.MESSAGE_RET_ONLINE_FRIENDS);
                     mes.setContent(ManageServerConnectClientThread.retOnlineUser());
-                    //send mes back to client
-                    ObjectOutputStream oos = new ObjectOutputStream(ManageServerConnectClientThread.getServerConnectClientThread(userID).socket.getOutputStream());
+                    // Send the message back to the client
+                    ObjectOutputStream oos = new ObjectOutputStream(
+                            ManageServerConnectClientThread.getServerConnectClientThread(userID).socket.getOutputStream());
                     oos.writeObject(mes);
                 }
 
-                //logout
+                // Handling client logout
                 if (message.getMesType().equals(MessageType.MESSAGE_CLIENT_EXIT)) {
                     ManageServerConnectClientThread.removeServerConnectClientThread(message.getSender());
                     socket.close();
-                    System.out.println("client " + userID + " logout");
+                    System.out.println("Client " + userID + " has logged out.");
                     break;
                 }
 
-                //receive private message request from client and send to target getter
+                // Processing private message requests from clients and forwarding to the intended recipient
                 if(message.getMesType().equals(MessageType.MESSAGE_COMM_MES)){
-                    //find the getter's socket on the server side from threads collection hm
+                    // Find the recipient's socket on the server using the collection of threads
                     ServerConnectClientThread receiverThread = ManageServerConnectClientThread
                             .getServerConnectClientThread(message.getGetter());
-                    System.out.println(receiverThread.userID + " is already to receive a message...");
-                    //send the message to getter
+                    System.out.println(receiverThread.userID + " is ready to receive a message.");
+                    // Send the message to the recipient
                     ObjectOutputStream oos = new ObjectOutputStream(receiverThread.socket.getOutputStream());
                     oos.writeObject(message);
-                    System.out.println("send the message to receiver "+ receiverThread.userID);
+                    System.out.println("Message sent to the recipient " + receiverThread.userID);
                 }
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -63,3 +63,4 @@ public class ServerConnectClientThread extends Thread {
         }
     }
 }
+
